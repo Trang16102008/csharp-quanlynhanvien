d09c7e63fd5b867f0df85e4bb9e5acacd7706259
ADMIN <ADMIN@LAB1-MAY24>
2024-06-10 15:34:42 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace csharpquanlynhanvien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsbketqua = new System.Windows.Forms.ListBox();
+            this.txtso = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsbketqua
+            // 
+            this.lsbketqua.FormattingEnabled = true;
+            this.lsbketqua.Location = new System.Drawing.Point(17, 162);
+            this.lsbketqua.Name = "lsbketqua";
+            this.lsbketqua.Size = new System.Drawing.Size(756, 212);
+            this.lsbketqua.TabIndex = 0;
+            // 
+            // txtso
+            // 
+            this.txtso.Location = new System.Drawing.Point(71, 116);
+            this.txtso.Name = "txtso";
+            this.txtso.Size = new System.Drawing.Size(247, 20);
+            this.txtso.TabIndex = 1;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(339, 85);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(138, 51);
+            this.btnthem.TabIndex = 2;
+            this.btnthem.Text = "thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "quản lý nhân viên";
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(483, 85);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(148, 53);
+            this.btnxoa.TabIndex = 4;
+            this.btnxoa.Text = "xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(360, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "nhập họ và tên";
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(637, 85);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(148, 53);
+            this.btnsua.TabIndex = 7;
+            this.btnsua.Text = "sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txtso);
+            this.Controls.Add(this.lsbketqua);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbketqua;
+        private System.Windows.Forms.TextBox txtso;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharpquanlynhanvien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            lsbketqua.Items.Add("Nguyễn Thị Phước\tAn");
+            lsbketqua.Items.Add("Trà Hoàng\tAn");
+            lsbketqua.Items.Add("Trần Lê Thanh\tBảo");
+            lsbketqua.Items.Add("Nguyễn Thị Hồng\tĐào");
+            lsbketqua.Items.Add("Nguyễn Văn\tDinh");
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            lsbketqua.Items.RemoveAt(lsbketqua.SelectedIndex);
+        }
+
+        private void btnSUA_Click(object sender, EventArgs e)
+        {
+            if (lsbketqua.SelectedIndex < 0) return;
+            lsbketqua.Items[lsbketqua.SelectedIndex] = txtso.Text;
+        }
+    }
+}
